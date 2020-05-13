3801e9022d664992493c31dbc7b80980b84f7557
KangJunewoo <brianjune@naver.com>
2020-05-13 18:51:34 +0900

    bcrypt, jsonwebtoken으로 암호화하기

--- ch2_server/routes/users.js
+++ ch2_server/routes/users.js
@@ -1,62 +1,139 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const UserModel = require('../model/User');
+const bcrypt = require('bcrypt');
+const jsonwebtoken = require('jsonwebtoken');
 
 router.post('/login', (req,res,next)=>{
   // object destructuring 문법.
   const {id, pw} = req.body;
   
   if(!id){
-    const error = new Error('user not found');
-    error.status = 404;
+    const error = new Error('bad request');
+    error.status = 400;
     return next(error);
     
     // 아래 방법은 에러처리 미들웨어를 사용하지 않는 코드.
     // return res.status(404).json({msg:'user not found'});
 
   } else if(!pw){
-    const error = new Error('invalid password');
-    error.status = 401;
+    const error = new Error('bad request');
+    error.status = 400;
     return next(error);
   }
   
-  //무사히 넘어가면
-  res.json({token:pw});
-})
+  return next();
+});
+router.post('/login', (req,res,next)=>{
+  // object destructuring 문법.
+  const {id, pw} = req.body;
+  const OnError=(error)=>{
+    return next(error);
+  }
+  const comparePassword=(user)=>{
+    if(!user){
+      const error = new Error('user not found');
+      error.status=404;
+      return next(error);
+    };
+    req.SearchUser=user;
+    return bcrypt.compare(pw, user.password);
+  };
+  const compareResultResponse = (isValid)=>{
+    if(isValid){
+      return next();
+    }
+    const error=new Error('invalid password');
+    error.status=401;
+    return next(error);
+  };
+  
+  UserModel.findOne({id:id}).select({id:1,password:1,CreatedAt:1})
+    .then(comparePassword)
+    .then(compareResultResponse)
+    .catch(OnError);
+});
+router.post('/login', (req,res,next)=>{
+  const options = {
+    algorithm:"HS256",
+    expiresIn:"10000",
+    issuer:"http://127.0.0.1",
+  };
+  const cert = "secret";
+  const plainObject = req.SearchUser.toObject({getters:true});
+  jsonwebtoken.sign(plainObject, cert, options, (err, token)=>{
+    if(err){
+      return next(err);
+    }
+    req.CreatedToken = token;
+    return next();
+  });
+});
+router.post('/login', (req,res,next)=>{
+  const OnError = (error)=>{
+    return next(error);
+  }
+
+  const updateResultResponse = (updatedUser)=>{
+    req.SearchUser = updatedUser;
+    return next();
+  }
+  
+  req.SearchUser.set({token:req.CreatedToken});
+  req.SearchUser.save()
+    .then(updateResultResponse)
+    .catch(OnError)
+});
+router.post('/login', (req,res,next)=>{
+  res.json({token:req.CreatedToken});
+});
 
 //user 생성요청
 router.post('/', (req,res,next)=>{
-  /**
-   * 아래처럼 쓰는게 주석의 정석인가보다~ 그냥 한 번 써봄.
-   * user 생성요청
-   * body : id, pw
-   * method : post
-   * content-type : x-www-urlencoded, application/json
-   */
-  /**
-   TODO DB 모델갖고 실제 user 구현
-   */
   const {id,pw}=req.body;
 
-  if(id !== 'hi'){
+  if(!id){
     const error = new Error('bad request');
     error.status = 400;
     return next(error);
-  } else if(pw !== 'bye'){
+  } else if(!pw){
     const error = new Error('bad request');
     error.status = 400;
     return next(error);
   }
   
-  //지금은 /에 대한 라우터 미들웨어를 두 번 사용했지만, 또 콜백함수 인자를 줘서 합칠수도있다.
-  //}, (req,res,next)=>{res.json(req.CreatedUser);}도 가능. rout라는 string을 줘야 한다고 한다.
-  
-  const User={
-    id:id,
-    pw:pw,
+  const generateStrictPassword = (salt) =>{
+    return bcrypt.hash(pw, salt);
   };
-  req.CreatedUser = User;
-  next();
+  const createUser = (strictPassword)=>{
+    const User = new UserModel({
+      id:id,
+      password:strictPassword
+    });
+    req.CreatedUser=User;
+    next();
+  };
+  const OnError = (error)=>{
+    return next(error);
+  }
+  bcrypt.genSalt(13)
+    .then(generateStrictPassword)
+    .then(createUser)
+    .catch(OnError);
+
+});
+
+router.post('/', (req,res,next)=>{
+  const OnError = (error)=>{
+    return next(error);
+  }
+  req.CreatedUser.save()
+    .then((user)=>{
+      req.CreatedUser=user;
+      return next();
+    })
+    .catch(OnError)
 });
 
 router.post('/', (req,res,next)=>{
